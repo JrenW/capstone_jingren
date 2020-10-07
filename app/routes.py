cef997bf55cf6e9cd6760cc61e846505bb499fc5
Jingren Wang <jingren.wang@minerva.kgi.edu>
2020-10-07 20:17:22 +0800

    build caption searcher
    build public routing to the caption search space, can now return {timestamp : caption} upon a keyword & number of search results by user input, for a "CC-ed" YouTube link (*CC=a transcript is available in YouTube transcript API )

--- app/routes.py
+++ app/routes.py
@@ -8,20 +8,64 @@ import json
 FILE_FOLDER = os.path.join('static', 'uploads')
 app.config['UPLOAD_FOLDER'] = FILE_FOLDER   # flask app configure files to be uploaded
 
+# set secret key for the session
+import secrets
+app.secret_key = secrets.token_urlsafe(16)
+
+
+@app.route('/cap_search', methods=['GET','POST'])
+def captionSearch():
+    if request.method == 'POST':
+        # fetch user input
+        keyword = request.form.get('keyword') # required
+        num_results = int(request.form.get('num_results')) # required
+        YouTube_URL = session['input_url']
+
+        # check necessary inputs to start a search
+        if None not in (YouTube_URL, keyword, num_results):
+            # search caption and display
+            captions = search_cap(YouTube_URL, keyword, num_results)
+            autoplay_link = ssession['input_url']+
+            # ref: https://stackoverflow.com/questions/7281765/how-to-embed-an-autoplaying-youtube-video-in-an-iframe
+
+            return render_template('main_page.html',input_url=session['input_url'],
+                            keyword=keyword, num_results=str(num_results),
+                            route='capSearch_1', captions=captions,
+                            autoplay_link=)
+
+                # return render_template('main_page.html', captions=captions,
+                #         keyword=keyword, num_results=num_results)
+
+        else:
+            return render_template('main_page.html',input_url=session['input_url'],
+                            keyword=keyword, num_results=str(num_results),
+                            notes="either keyword or user_url is missing...",
+                            route='capSearch_2')
+
+    else:
+        return render_template('main_page.html', input_url=session['input_url'], route='capSearch_3')
+
+
+
 @app.route('/', methods=['GET','POST'])
 def renderMain():
     eye_icon = os.path.join(app.config['UPLOAD_FOLDER'],'images', 'eye_icon.png' )
 
-    YouTube_URL = 'https://www.youtube.com/watch?v=2itlXnURSP4'
-    keyword = 'politics'
-    num_results = 3
+    # request data from user input
+    if request.method == 'POST':
+        input_url= request.form.get('videoURL')
+        #user_url = input_url
+        if input_url:
+            # store user url for the current session before redirecting
+            session['input_url'] = input_url
+            return redirect(url_for('captionSearch', input_url=input_url))
 
-    # get a list of time-caption dictionaries from user input
-    captions = search_cap(YouTube_URL, keyword, num_results)
+        else:
+            return render_template('home.html')
+    else:
+        return render_template('home.html')
 
 
-    return render_template('main_page.html',
-    icon_image=eye_icon,  captions=captions)
 
 @app.route('/admin/<name>')
 def check_admin(name):
